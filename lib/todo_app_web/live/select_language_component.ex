2cd5fc297cbaa25ed598b08f433b7f500116f128
Gustavo <gustavo.fernandesmoreira@student.jade-hs.de>
2025-06-13 13:32:10 +0200

    Add language options

--- lib/todo_app_web/live/select_language_component.ex
+++ lib/todo_app_web/live/select_language_component.ex
@@ -23,6 +23,33 @@ defmodule TodoAppWeb.SelectLanguageComponent do
       >
         {gettext("English")}
       </button>
+            <button
+        class={[
+          "bg-taDark-dark px-1.5 py-0.5 text-white text-xs font-extralight rounded-lg hover:scale-105 shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.8)]",
+          Gettext.get_locale(TodoAppWeb.Gettext) == "de_DE" && "bg-taDark-light"
+        ]}
+        phx-click="german"
+      >
+        {gettext("German")}
+      </button>
+            <button
+        class={[
+          "bg-taDark-dark px-1.5 py-0.5 text-white text-xs font-extralight rounded-lg hover:scale-105 shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.8)]",
+          Gettext.get_locale(TodoAppWeb.Gettext) == "fr_FR" && "bg-taDark-light"
+        ]}
+        phx-click="french"
+      >
+        {gettext("French")}
+      </button>
+            <button
+        class={[
+          "bg-taDark-dark px-1.5 py-0.5 text-white text-xs font-extralight rounded-lg hover:scale-105 shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.8)]",
+          Gettext.get_locale(TodoAppWeb.Gettext) == "pt_BR" && "bg-taDark-light"
+        ]}
+        phx-click="portuguese"
+      >
+        {gettext("Portuguese")}
+      </button>
     </div>
     """
   end
